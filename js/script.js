6960e19355817c078e8daa571b280ce28f4f4f8c
default <devin.boucher@stu.ocsb.ca>
2025-03-18 14:02:53 +0000

    website added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Do basic math.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + firstName + ", " + userAge + "."
 }
